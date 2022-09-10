339f4cbcf8320b7ded66e28f80481d9cf95bae83
ZeeCaptcha <ayomiposijimoh@gmail.com>
2022-09-10 04:09:09 +0100

    ALX is fun

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - main block
  * Description: Write a program that prints all possible
  * different combinations of two digits
  * Return: 0
 */
-
 int main(void)
 {
 int c;
